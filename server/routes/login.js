27d8a8b8580ae41d12f3ac612fa5cfd50754be08
yihu55 <veronicayi@msn.com>
2022-03-24 22:09:35 +0100

    bcrypt password in mysql

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/User');
 
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
-  const user = await User.findOne({ where: { username } }).catch((err) => {
-    console.log('Error', err);
-  });
-
+  const user = await User.findOne({ where: { username } });
+  //.catch((err) => {
+  //   console.log('Error', err);
+  // });
+  // if (user && (await bcrypt.compare(password, user.password))) {
+  //   const jwtToken = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+  //   res.json({ message: 'Welcome back', token: jwtToken });
+  // }
+  try {
+    const match = await bcrypt.compare(password, user.password);
+    const jwtToken = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+    if (match) {
+      return res.json({ message: 'Welcome back', token: jwtToken });
+    }
+  } catch (err) {
+    res.json({ message: err });
+  }
   if (!user) {
     return res.json({ message: 'User and password does not match!' });
   }
@@ -19,8 +33,8 @@ router.post('/login', async (req, res) => {
     return res.json({ message: 'password does not match' });
   }
 
-  const jwtToken = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
-  res.json({ message: 'Welcome back', token: jwtToken });
+  // const jwtToken = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+  // res.json({ message: 'Welcome back', token: jwtToken });
 });
 
 module.exports = router;
--- server/routes/register.js
+++ server/routes/register.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/User');
 
 router.get('/register', (req, res) => {
@@ -20,7 +22,11 @@ router.post('/register', async (req, res) => {
     return res.json({ message: 'User already exists!' });
   }
 
-  const newUser = new User({ username, password });
+  //hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const newUser = new User({ username: username, password: hashedPassword });
   const savedUser = await newUser.save().catch((err) => {
     console.log('Error', err);
   });
