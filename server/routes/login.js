7b410f731135d594409efd0d5c5870a9938b45f7
yihu55 <veronicayi@msn.com>
2022-03-24 22:21:23 +0100

    deleted commented text

--- server/routes/login.js
+++ server/routes/login.js
@@ -7,15 +7,8 @@ const User = require('../models/User');
 
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
-
   const user = await User.findOne({ where: { username } });
-  //.catch((err) => {
-  //   console.log('Error', err);
-  // });
-  // if (user && (await bcrypt.compare(password, user.password))) {
-  //   const jwtToken = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
-  //   res.json({ message: 'Welcome back', token: jwtToken });
-  // }
+
   try {
     const match = await bcrypt.compare(password, user.password);
     const jwtToken = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
@@ -32,9 +25,6 @@ router.post('/login', async (req, res) => {
   if (user.password !== password) {
     return res.json({ message: 'password does not match' });
   }
-
-  // const jwtToken = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
-  // res.json({ message: 'Welcome back', token: jwtToken });
 });
 
 module.exports = router;
